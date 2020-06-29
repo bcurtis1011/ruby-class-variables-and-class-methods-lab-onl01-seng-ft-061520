6ada704883ba91089c6a650ecfecc59b457ac220
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-29 20:45:41 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  
+   attr_reader :name, :artist, :genre
+   @@artist = []
+   @@count = 0
+  
+  def initialize(name, artist, genre)
+    #@@artist << artist
+    @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+  
+  def name=(name)
+    @name
+  end
+  
+  def artist=(artist)
+    @artist
+    @@artist << artist
+  end
+  
+  def genre=(genre)
+    @genre
+  end
+end
